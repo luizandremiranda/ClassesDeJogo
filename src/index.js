dd7ed6106196963bc73b8a80609c7bd7a7828acb
luizandremiranda <luizmiranda.lasm@gmail.com>
2024-07-01 21:06:53 -0300

    Finalizando aatividade

---
+++ src/index.js
@@ -0,0 +1,25 @@
+class heroi {
+    constructor (nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+ataque(){
+    let arma = ""
+    if this.tipo = guerreiro {
+        arma = "espada"        
+    }
+    else if this.tipo = mago {
+        arma = "magia"
+    }
+    else if this.tipo = monge {
+        arma = "artes marciais"
+    }
+    else if this.tipo = ninja {
+        arma = "shuriken"
+    }
+}
+print (`o ${tipo} atacou usando ${ataque}`)
+}
+
+const player = new.heroi(Luiz, 31, guerreiro)
